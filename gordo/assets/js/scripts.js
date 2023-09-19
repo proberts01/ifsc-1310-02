d55fea03e87f0b8bd833136fcf35430afeacb935
Rusty Russian <115586896+rustyrussian@users.noreply.github.com>
2023-09-19 15:14:15 +0000

    Removing the css and js from interacting with gordo assignment from testing with AI.

--- gordo/assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-function toggleStyles() {
-    const stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
-    stylesheets.forEach((stylesheet) => {
-        stylesheet.disabled = !stylesheet.disabled;
-    });
-}
